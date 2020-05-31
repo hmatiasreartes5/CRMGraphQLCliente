05a9b566856a5fb486606a1fce7006b82a6f3839
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-05-31 20:41:15 -0300

    Crando login y nueva cuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,11 +1,136 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 const NuevaCuenta = () => {
+
+    //validacion del formulario --- Muy similar al useState osea hace lo mismo pero me permite la validacion de inputs
+    const formik = useFormik({
+        initialValues: {
+            nombre:'',
+            apellido:'',
+            email:'',
+            password:''
+        },
+        validationSchema: Yup.object({
+            nombre: Yup.string().required('El nombre es oblidatorio'),
+            apellido: Yup.string().required('El apellido es oblidatorio'),
+            email: Yup.string()
+                        .email('El email no es valido')
+                        .required('El email es oblidatorio'),
+            password: Yup.string()
+                        .required('El password es oblidatorio')
+                        .min(6,'El password debe ser de al menos 6 caracteres')
+        }),
+        onSubmit: valores => {
+            console.log('enviando');
+            console.log(valores);   
+        }
+    });
+
     return ( 
         <>
             <Layout>
                 <h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
+                <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-sm">   
+                        <form
+                            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                            onSubmit={formik.handleSubmit}
+                        >
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                    Nombre 
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="nombre"
+                                    type="text"
+                                    placeholder="Nombre Usuario"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+
+                            {formik.touched.nombre && formik.errors.nombre ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                    <p className="font-bold">Error</p>
+                                    <p>{formik.errors.nombre}</p>
+                                </div>
+                            ):null}
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                                    Apellido
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="apellido"
+                                    type="text"
+                                    placeholder="Apellido Usuario"
+                                    value={formik.values.apellido}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+
+                            {formik.touched.apellido && formik.errors.apellido ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                    <p className="font-bold">Error</p>
+                                    <p>{formik.errors.apellido}</p>
+                                </div>
+                            ):null}
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                    Email
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email User"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+
+                            {formik.touched.email && formik.errors.email ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                    <p className="font-bold">Error</p>
+                                    <p>{formik.errors.email}</p>
+                                </div>
+                            ):null}
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+
+                            {formik.touched.password && formik.errors.password ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                    <p className="font-bold">Error</p>
+                                    <p>{formik.errors.password}</p>
+                                </div>
+                            ):null}
+
+                            <input
+                                type="submit"
+                                className="bg-gray-800 w-full mt-5 p-2 text-white uppercas hover:cursor-pointer hover:bg-gray-900"
+                                value="Crear Cuenta"
+                            />
+                        </form>
+                    </div>
+                </div>
             </Layout>
         </>
      );
